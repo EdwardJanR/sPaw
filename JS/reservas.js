9bf83bb27b2953f856f05d9dc36cf64907d5dd27
juanrodriguezs1503 <juan.rodriguezs1503@gmail.com>
2025-12-05 18:55:50 -0500

    Formato fecha actualizado

--- JS/reservas.js
+++ JS/reservas.js
@@ -1,7 +1,9 @@
-// Flatpickr
 flatpickr("#fecha", {
-    dateFormat: "d-m-Y",
-    minDate: "today",
-    disableMobile: true
+    locale: "es",          
+    minDate: "today",      
+     maxDate: new Date().fp_incr(90),
+    dateFormat: "d-m-Y",  
+    altInput: true,       
+    altFormat: "F j, Y",  
+    allowInput: true
 });
-
