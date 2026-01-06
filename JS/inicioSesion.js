95e7adb75f7972ca796674900e40f7dcc2756c21
AngieLi98 <liceth5262@gmail.com>
2026-01-05 19:39:53 -0500

    validacion y verificaciones

---
+++ JS/inicioSesion.js
@@ -0,0 +1,77 @@
+function validaciones() {
+  limpiarValidaciones();
+
+  const email = document.getElementById("email").value.trim();
+  const contrasena = document.getElementById("password").value.trim();
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    mostrarValidaciones("email", "Por favor ingresa un email válido.");
+    return false;
+  }
+
+  if (contrasena.length <= 8) {
+    mostrarValidaciones(
+      "password",
+      "La contraseña debe tener mínimo 8 caracteres"
+    );
+    return false;
+  }
+
+  verificarUsuario();
+  limpiarFormulario();
+  return true;
+}
+
+function mostrarValidaciones(id, mensaje) {
+  const field = document.getElementById(id);
+  const formFloating = field.closest(".form-floating");
+
+  const errorElement = document.createElement("div");
+  errorElement.className = "error-message text-danger mt-1 small";
+  errorElement.textContent = mensaje;
+
+  formFloating.appendChild(errorElement);
+
+  field.classList.add("is-invalid");
+}
+
+function limpiarValidaciones() {
+  document
+    .querySelectorAll(".error-message")
+    .forEach((error) => error.remove());
+  document
+    .querySelectorAll(".is-invalid")
+    .forEach((field) => field.classList.remove("is-invalid"));
+}
+
+function verificarUsuario() {
+  const email = document.getElementById("email").value.trim();
+  const contrasena = document.getElementById("password").value.trim();
+
+  const usuariosRegistrados =
+    JSON.parse(localStorage.getItem("infoRegistro")) || [];
+
+  const usuarioEncontrado = usuariosRegistrados.find(
+    (usuario) => usuario.correo === email && usuario.contrasena === contrasena
+  );
+
+  if (usuarioEncontrado) {
+    // Guardar usuario logueado
+    const usuarioActivo = {
+      nombre: usuarioEncontrado.nombre,
+      apellidos: usuarioEncontrado.apellidos,
+    };
+
+    localStorage.setItem("usuarioActivo", JSON.stringify(usuarioActivo));
+
+    alert("¡Inicio de sesión exitoso!");
+    window.location.href = "../index.html";
+  } else {
+    alert("Usuario o contraseña incorrectos.");
+  }
+}
+
+function limpiarFormulario() {
+  document.getElementById("formRegistro").reset();
+}
