f411b61816ca4b17ddbb0c85e4e0373508fd1c65
EdwardJanR <ejrobledop@gmail.com>
2026-01-20 09:17:10 -0500

    actualizacion reservas

--- JS/reservas.js
+++ JS/reservas.js
@@ -1,187 +1,201 @@
-let picker = flatpickr("#fechaReserva", {
-    locale: "es",
-    minDate: "today",
-    maxDate: new Date().fp_incr(90),
-    dateFormat: "d-m-Y",
-    altInput: false,
-    allowInput: false,
-    disableMobile: false,
+document.addEventListener('DOMContentLoaded', function () {
+
+    const btnReservar = document.getElementById('btnReservar');
+    const inputNombreMascota = document.getElementById('nombreMascota');
+    const inputTamanoMascota = document.getElementById('tamanoMascota');
+    const inputNombreGroomer = document.getElementById('nombreGroomer');
+    const inputNombreServicio = document.getElementById('nombreServicio');
+    const inputFechaReserva = document.getElementById('fechaReserva');
+    const inputHoraReserva = document.getElementById('horaReserva');
 
 
-    onChange: function (selectedDates, dateStr, instance) {
+    flatpickr.localize(flatpickr.l10ns.es);
 
-        document.getElementById('fechaReserva').dispatchEvent(new Event('input'));
-        document.getElementById('fechaReserva').dispatchEvent(new Event('change'));
+
+    let picker = flatpickr("#fechaReserva", {
+        locale: "es",
+        minDate: "today",
+        maxDate: new Date().fp_incr(90),
+        dateFormat: "d-m-Y",
+        altInput: false,
+        allowInput: false,
+        disableMobile: false,
+
+
+        onChange: function (selectedDates, dateStr, instance) {
+
+            document.getElementById('fechaReserva').dispatchEvent(new Event('input'));
+            document.getElementById('fechaReserva').dispatchEvent(new Event('change'));
+        }
+    });
+
+
+    btnReservar.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        limpiarVal();
+
+    const nombreMascota = inputNombreMascota.value.trim();
+    const tamanoMascota = inputTamanoMascota.value.trim();
+    const nombreGroomer = inputNombreGroomer.value.trim();
+    const nombreServicio = inputNombreServicio.value.trim();
+    const fechaReserva = inputFechaReserva.value.trim();
+    const horaReserva = inputHoraReserva.value.trim();
+
+
+        if (nombreMascota.length < 2 || !/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(nombreMascota)) {
+            mostrarVal('nombreMascota', 'El nombre de la mascota debe ser alfabético y mínimo dos caracteres');
+            return false;
+        }
+        if (tamanoMascota === "") {
+            mostrarVal('tamanoMascota', 'Por favor selecciona el tamaño de la mascota');
+            return false;
+        }
+        if (nombreGroomer === "") {
+            mostrarVal('nombreGroomer', 'Por favor selecciona un groomer');
+            return false;
+        }
+        if (nombreServicio === "") {
+            mostrarVal('nombreServicio', 'Por favor selecciona un servicio');
+            return false;
+        }
+        if (fechaReserva === "") {
+            mostrarVal('fechaReserva', 'Por favor selecciona una fecha para la reserva');
+            return false;
+        }
+
+        if (horaReserva < "08:00" || horaReserva > "18:00" || horaReserva === "") {
+            mostrarVal('horaReserva', 'Debe seleccionar una hora entre 08:00 AM y 06:00 PM');
+            return false;
+        }
+
+        mostrarAlerta('success', '<strong>¡Éxito!</strong> Todos los campos son válidos. Enviando formulario...');
+
+        setTimeout(() => {
+            document.getElementById('formContactenos').submit();
+        }, 1500);
+
+        procesarReserva({
+            nombreMascota,
+            tamanoMascota,
+            fechaReserva,
+            horaReserva
+        });
+        return true;
+    });
+
+    function procesarReserva(datosReserva) {
+
+        let listaReservas = JSON.parse(localStorage.getItem('listaReservas')) || { contador: 0, items: [] };
+
+        listaReservas.contador++;
+        const nuevoId = listaReservas.contador;
+
+        const infoReservas = {
+            idReserva: nuevoId,
+            nombreServicio: inputNombreServicio.options[inputNombreServicio.selectedIndex].text,
+            nombreMascota: datosReserva.nombreMascota,
+            tamanoMascota: datosReserva.tamanoMascota,
+            nombreGroomer: inputNombreGroomer.options[inputNombreGroomer.selectedIndex].text,
+            fechaReserva: datosReserva.fechaReserva,
+            horaReserva: datosReserva.horaReserva
+        };
+
+        console.log(JSON.stringify(infoReservas));
+
+        listaReservas.items.push(infoReservas);
+
+        localStorage.setItem("listaReservas", JSON.stringify(listaReservas));
+
+        //Aleta para el usuario
+        mostrarAlerta('success', '<strong>¡Reserva exitosa!</strong> Tu reserva ha sido guardada correctamente.');
+        limpiarFormulario();
+        mostrarReservas();
     }
-});
 
+    function mostrarReservas() {
+        const reservas = JSON.parse(localStorage.getItem("listaReservas")) || { contador: 0, items: [] };
+        const contenedorReservas = document.getElementById("servicio-reservado");
 
-function validarInfoReserva() {
+        contenedorReservas.innerHTML = "";
 
-    limpiarVal();
-    const nombreMascota = document.getElementById('nombreMascota').value.trim();
-    const tamanoMascota = document.getElementById('tamanoMascota').value.trim();
-    const nombreGroomer = document.getElementById('nombreGroomer').value.trim();
-    const nombreServicio = document.getElementById('nombreServicio').value.trim();
-    const fechaReserva = document.getElementById('fechaReserva').value.trim();
-    const horaReserva = document.getElementById('horaReserva').value.trim();
+        if (reservas.items.length === 0) {
+            contenedorReservas.innerHTML = "<p>No hay reservas realizadas.</p>";
+            return;
+        }
 
-    if (nombreMascota.length < 2 || !/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(nombreMascota)) {
-        mostrarVal('nombreMascota', 'El nombre de la mascota debe ser alfabético y mínimo dos caracteres');
-        return false;
-    }
-    if (tamanoMascota === "") {
-        mostrarVal('tamanoMascota', 'Por favor selecciona el tamaño de la mascota');
-        return false;
-    }
-    if (nombreGroomer === "") {
-        mostrarVal('nombreGroomer', 'Por favor selecciona un groomer');
-        return false;
-    }
-    if (nombreServicio === "") {
-        mostrarVal('nombreServicio', 'Por favor selecciona un servicio');
-        return false;
-    }
-    if (fechaReserva === "") {
-        mostrarVal('fechaReserva', 'Por favor selecciona una fecha para la reserva');
-        return false;
+        reservas.items.forEach(reseva => {
+            const reservaCard = document.createElement('reserva-card');
+            reservaCard.setAttribute('nombreServicio', reseva.nombreServicio);
+            reservaCard.setAttribute('nombreMascota', reseva.nombreMascota);
+            reservaCard.setAttribute('tamanoMascota', reseva.tamanoMascota);
+            reservaCard.setAttribute('nombreGroomer', reseva.nombreGroomer);
+            reservaCard.setAttribute('fechaReserva', reseva.fechaReserva);
+            reservaCard.setAttribute('horaReserva', reseva.horaReserva);
+            reservaCard.setAttribute('idReserva', reseva.idReserva);
+            contenedorReservas.appendChild(reservaCard);
+        }
+        );
     }
 
-    if (horaReserva < "08:00" || horaReserva > "18:00" || horaReserva === "") {
-        mostrarVal('horaReserva', 'Debe seleccionar una hora entre 08:00 AM y 06:00 PM');
-        return false;
+    function mostrarVal(id, mensaje) {
+        const field = document.getElementById(id);
+        const formFloating = field.closest('.form-floating');
+
+        const errorElement = document.createElement('div');
+        errorElement.className = 'error-message text-danger mt-1 small';
+        errorElement.textContent = mensaje;
+
+        formFloating.appendChild(errorElement);
+
+        field.classList.add('is-invalid');
     }
 
-    mostrarAlerta('success', '<strong>¡Éxito!</strong> Todos los campos son válidos. Enviando formulario...');
-            
-    setTimeout(() => {
-        document.getElementById('formContactenos').submit();
-    }, 1500); 
-
-    guardarInformacion();
-    return true;
-}
-
-function mostrarVal(id, mensaje) {
-    const field = document.getElementById(id);
-    const formFloating = field.closest('.form-floating');
-
-    const errorElement = document.createElement('div');
-    errorElement.className = 'error-message text-danger mt-1 small';
-    errorElement.textContent = mensaje;
-
-    formFloating.appendChild(errorElement);
-
-    field.classList.add('is-invalid');
-}
-
-function limpiarVal() {
-    document.querySelectorAll('.error-message').forEach(error => error.remove());
-    document.querySelectorAll('.is-invalid').forEach(field => field.classList.remove('is-invalid'));
-}
+    function limpiarVal() {
+        document.querySelectorAll('.error-message').forEach(error => error.remove());
+        document.querySelectorAll('.is-invalid').forEach(field => field.classList.remove('is-invalid'));
+    }
 
 
-function guardarInformacion() {
-    let nombreServicio = document.getElementById("nombreServicio");
-    let nombreMascota = document.getElementById("nombreMascota").value;
-    let tamanoMascota = document.getElementById("tamanoMascota").value;
-    let nombreGroomer = document.getElementById("nombreGroomer");
-    let fechaReserva = document.getElementById("fechaReserva").value;
-    let horaReserva = document.getElementById("horaReserva").value;
 
-    //Se obtiene la descripción del valor seleccionado en el select nombreServicio
-    let servicioSeleccionado = nombreServicio.options[nombreServicio.selectedIndex].text;
-    //Se obtiene la descripción del valor seleccionado en el select nombreGroomer
-    let groomerSeleccionado = nombreGroomer.options[nombreGroomer.selectedIndex].text;
 
-    //let listaReservas = JSON.parse(localStorage.getItem("listaReservas")) || [];
-    let listaReservas = JSON.parse(localStorage.getItem('listaReservas')) || { contador: 0, items: [] };
+    function mostrarAlerta(mensaje, tipo) {
+        const alertContainer = document.getElementById('alertContainer');
 
-    listaReservas.contador++;
-    const nuevoId = listaReservas.contador;
-
-    const infoReservas = {
-        idReserva: nuevoId,
-        nombreServicio: servicioSeleccionado,
-        nombreMascota: nombreMascota,
-        tamanoMascota: tamanoMascota,
-        nombreGroomer: groomerSeleccionado,
-        fechaReserva: fechaReserva,
-        horaReserva: horaReserva
-    };
-
-    console.log(JSON.stringify(infoReservas));
-
-    listaReservas.items.push(infoReservas);
-
-    localStorage.setItem("listaReservas", JSON.stringify(listaReservas));
-
-    //Aleta para el usuario
-    mostrarAlerta('<strong>¡Éxito!</strong> Todos los campos son válidos. Enviando formulario...','success');
-    limpiarFormulario();
-    mostrarReservas();
-}
-
-function mostrarAlerta(mensaje, tipo) {
-    const alertContainer = document.getElementById('alertContainer');
-            
-    const alerta = document.createElement('div');
-    alerta.className = `alert alert-${tipo} alert-dismissible fade show`;
-    alerta.role = 'alert';
-    alerta.innerHTML = `
+        const alerta = document.createElement('div');
+        alerta.className = `alert alert-${tipo} alert-dismissible fade show`;
+        alerta.role = 'alert';
+        alerta.innerHTML = `
         ${mensaje}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     `;
-            
-    alertContainer.innerHTML = '';
-    alertContainer.appendChild(alerta);
-            
-    setTimeout(() => {
-        alerta.classList.remove('show');
-        setTimeout(() => alerta.remove(), 150);
-    }, 3000);
-}
+        alertContainer.innerHTML = '';
+        alertContainer.appendChild(alerta);
 
-function eliminarReserva(idAEliminar) {
-    let data = JSON.parse(localStorage.getItem("listaReservas"));
+        setTimeout(() => {
+            alerta.classList.remove('show');
+            setTimeout(() => alerta.remove(), 150);
+        }, 3000);
+    }
 
-    // filtrar los items que NO sean el id a eliminar
-    data.items = data.items.filter(item => item.idReserva !== idAEliminar);
+    function eliminarReserva(idAEliminar) {
+        let data = JSON.parse(localStorage.getItem("listaReservas"));
 
-    // guardar nuevamente
-    localStorage.setItem("listaReservas", JSON.stringify(data));
+        // filtrar los items que NO sean el id a eliminar
+        data.items = data.items.filter(item => item.idReserva !== idAEliminar);
+
+        // guardar nuevamente
+        localStorage.setItem("listaReservas", JSON.stringify(data));
+
+        mostrarReservas();
+    }
+
+
+
+    function limpiarFormulario() {
+        document.getElementById("formReserva").reset();
+    }
 
     mostrarReservas();
-}
 
-function mostrarReservas() {
-    const data = JSON.parse(localStorage.getItem("listaReservas")) || { contador: 0, items: [] };
-    const contenedor = document.getElementById("servicio-reservado");
-
-    contenedor.innerHTML = "";
-
-    data.items.forEach(p => {
-        const div = document.createElement("div");
-        div.className = "agendar col-12 col-xl-5 rounded-4 justify-content-center m-2 py-5";
-        div.innerHTML = `
-            <div>
-                <h2 class="subtitulo mb-2">${p.nombreServicio}</h2>
-                <p><b>Nombre de la mascota:</b> ${p.nombreMascota}</p>
-                <p><b>Tamaño:</b> ${p.tamanoMascota}</p>
-                <p><b>Groomer escogido:</b> ${p.nombreGroomer}</p>
-                <p><b>Fecha:</b> ${p.fechaReserva}</p>
-                <p><b>Hora:</b> ${p.horaReserva}</p>
-                <p><i class="bi bi-trash" onclick="eliminarReserva(${p.idReserva})"></i></p>
-                <a class="boton-login" href="../HTML/servicios.html">Volver a servicios</a>
-            </div>
-        `;
-        contenedor.appendChild(div);
-    });
-}
-
-
-function limpiarFormulario() {
-    document.getElementById("formReserva").reset();
-}
-
-mostrarReservas();
+});
