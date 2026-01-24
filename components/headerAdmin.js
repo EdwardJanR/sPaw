d49640011001b531e6ec6960a5a99b803318b005
AngieLi98 <liceth5262@gmail.com>
2026-01-23 19:09:51 -0500

    cambios diseño

---
+++ components/headerAdmin.js
@@ -0,0 +1,30 @@
+class HeaderAdmin extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <nav class="navbar navbar-expand-lg menu">
+                <div class="container-fluid d-flex align-items-center">
+                    <a class="navbar-brand d-flex align-items-center" href="../index.html">
+                        <img class="logo" src="../../IMG/logo-row-02.png" alt="logo-sPaw">
+                    </a>
+
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 gap-4 pe-2">
+                            <li class="nav-item"><a class="nav-link" href="../../index.html">Inicio</a></li>
+                            <li class="nav-item"><a class="nav-link" href="../quienesSomos.html">Quienes somos</a></li>
+                            <li class="nav-item"><a class="nav-link" href="../servicios.html">Servicios</a></li>
+                            <li class="nav-item"><a class="nav-link" href="../contactenos.html">Contáctanos</a></li>
+                        </ul>
+                        <a href="../inicioSesion.html" class="boton-login">Iniciar Sesión</a>
+                    </div>
+                </div>
+            </nav>
+        `;
+    }
+}
+
+customElements.define("header-admin", HeaderAdmin);
